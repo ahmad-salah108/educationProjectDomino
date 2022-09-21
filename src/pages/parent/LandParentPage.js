470db3a109221e49dd2ea971c07cba35db0463a8
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-09-21 21:33:49 +0300

    new

--- src/pages/parent/LandParentPage.js
+++ src/pages/parent/LandParentPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ParentNavbar from '../../components/parent/ParentNavbar'
+import '../../assest/css/parent/landParentPage.css'
 
 export default function LandParentPage() {
   return (
-    <div>
+    <div className='land-parent'>
         <ParentNavbar/>
     </div>
   )
