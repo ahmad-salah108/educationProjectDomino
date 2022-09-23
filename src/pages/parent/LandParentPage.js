8ad674331f213344ef35226ca6bce9877bf4c121
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-09-23 09:19:44 +0300

    parent dashboard

--- src/pages/parent/LandParentPage.js
+++ src/pages/parent/LandParentPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ParentNavbar from '../../components/parent/ParentNavbar'
 import '../../assest/css/parent/landParentPage.css'
+import ParenttopBoxes from '../../components/parent/ParentTopBoxes'
 
 export default function LandParentPage() {
   return (
     <div className='land-parent'>
         <ParentNavbar/>
+        <div className='parent-land-content'>
+          <ParenttopBoxes/>
+        </div>
     </div>
   )
 }
